65b29671d38c9d2779344569318a3dcf82a97d4c
Ismail andaloussi <57640444+ismail1k@users.noreply.github.com>
2022-06-26 19:43:34 +0000

    Commit

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 Auth::routes([
     'login' => true,
-    'register' => true,
+    'register' => false,
     'reset' => false,
     'verify' => false,
 ]);
