d0f2092e21f20dc247a266cb14d7a04f8d615883
Ismail andaloussi <57640444+ismail1k@users.noreply.github.com>
2022-06-27 19:37:31 +0000

    Commit

---
+++ app/Http/Controllers/EventController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Player;
+use App\Models\Logger;
+
+class EventController extends Controller
+{
+    public function discord(Request $request){
+        if($request->action == 'left'){
+            Player::where('discordid', $request->discordid)->update([
+                'locked' => 1,
+            ]);
+            Logger::create([
+                'actionby' => 'BOT: discord-api Event',
+                'description' => 'Unwhitelist player. for reason: Member left server.',
+            ]);
+            return response()->json(['status' => 200]);
+        }
+        return response()->json(['status' => 404]);
+    }
+}
--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    public const HOME = '/';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
@@ -29,8 +29,7 @@ class RouteServiceProvider extends ServiceProvider
         $this->configureRateLimiting();
 
         $this->routes(function () {
-            Route::middleware('api')
-                ->prefix('api')
+            Route::prefix('api')
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
