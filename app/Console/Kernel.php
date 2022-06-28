ffdd0916d52b5820f8c6f6efe979be062933b01e
ismail <oneigcy@gmail.com>
2022-06-28 10:36:40 +0100

    Commit

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,9 +16,9 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('samp:restart')
-        ->timezone('Africa/Casablanca')
-        ->dailyAt('05:00');
+        // $schedule->command('samp:restart')
+        // ->timezone('Africa/Casablanca')
+        // ->dailyAt('05:00');
     }
 
     /**
